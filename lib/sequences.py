564f8eba1f98aa2890f28e4ed571d147019c9320
Joseph <josephylee72@gmail.com>
2023-10-23 14:35:52 -0700

    completed sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    result = []
+    a = 0
+    b = 1
+    c = 1
+    while (len(result) < length):
+        result.append(a)
+        a = b
+        b = c
+        c = a + b
+    print(result)
+
+print_fibonacci(10)
